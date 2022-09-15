a80b1d80722896ec197e373d93a7ccdd39a9b835
ShevaGO <chaklun73sheva@mail.ru>
2022-09-15 16:58:11 +0300

    dz1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,16 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let d = b**2-4*a*c;
+  let rootOne = -b/(2*a);
+  let rootTwo = (-b + Math.sqrt(d) )/(2*a);
+  let rootThree = (-b - Math.sqrt(d) )/(2*a);
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    arr.push(rootOne);
+  } else if (d > 0) {
+    arr.push(rootTwo,rootThree);
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
